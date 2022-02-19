af476706057eeeeb6991bdcf31b59dc664fefd01
Kelly Alzate <kejoh07@gmail.com>
2022-02-19 01:57:01 +0100

    Creación del componente SearchBar,
    Co-authored-by: Cris <crprmahdz@yahoo.com>
    Co-authored-by: Vanina <vaniwinnik@gmail.com>
    Co-authored-by: Mariana <marianasolquinteros@gmail.com>
    Co-authored-by: Kelly <kejoh07@gmail.com>
    Co-authored-by: Jesus <jbernalme@gmail.com>

---
+++ recetario/src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, ViewSafeArea, TextInput } from "react-native";
+
+const SearchBar = () => {
+    return (
+        <View>
+            <TextInput placeholder="What do you want to eat?"/>
+        </View>
+    )
+}
+
+export default SearchBar;
